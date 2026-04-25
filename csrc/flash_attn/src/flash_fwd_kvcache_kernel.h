d0f9b2668ad429dcfb45a696b5d91f74746af11a
FumeiYoruu <95714792+FumeiYoruu@users.noreply.github.com>
2026-04-25 00:48:11 -0700

    Update flash_fwd_kvcache_kernel.h

--- csrc/flash_attn/src/flash_fwd_kvcache_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kvcache_kernel.h
@@ -524,12 +524,11 @@ inline __device__ void compute_attn_kvcache_1rowblock(
     // -----------------------------------------------------------------------
     // Epilogue: normalise O, write to gmem, write LSE
     // -----------------------------------------------------------------------
-    // Debug: print rM/rL for thread 0 of warp 0
-    if (warp_id == 0 && lane_id == 0 && bidb == 0 && bidh == 0) {
-        printf("[kvcache dbg] bidb=%d bidh=%d m_block=%d n_block_max=%d "
-               "rM[0]=%f rM[1]=%f rL[0]=%f rL[1]=%f\n",
-               bidb, bidh, m_block, n_block_max,
-               rM[0], rM[1], rL[0], rL[1]);
+    // Debug: print rM/rL for thread 0
+    if (threadIdx.x == 0) {
+        printf("[dbg] bidb=%d bidh=%d m=%d n_max=%d seqk=%d rM0=%f rM1=%f rL0=%f rL1=%f O0=%f\n",
+               bidb, bidh, m_block, n_block_max, total_seqlen_k,
+               rM[0], rM[1], rL[0], rL[1], tOrO_float[0]);
     }
     for (int i = 0; i < 2; i++) {
         if (rL[i] != 0.0f) {
